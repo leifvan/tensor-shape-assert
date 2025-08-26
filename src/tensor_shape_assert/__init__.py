87a77c8507d93430421190478a75ade55ca0ce8b
Leif Van Holland <holland@cs.uni-bonn.de>
2025-08-26 16:36:17 +0000

    added scalartensor to global export

--- src/tensor_shape_assert/__init__.py
+++ src/tensor_shape_assert/__init__.py
@@ -1,1 +1,4 @@
-from .wrapper import check_tensor_shapes, ShapedTensor, get_shape_variables, assert_shape_here, set_global_check_mode
+from .wrapper import (
+    check_tensor_shapes, ShapedTensor, get_shape_variables, assert_shape_here,
+    set_global_check_mode, ScalarTensor
+)
